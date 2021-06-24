493ba20afff215c5e1be674dbd8c34daa5a68430
KutashiMA <kutashimuhammed@gmail.com>
2021-06-24 07:43:34 +0100

    all functions working, also added function to use all five functions at once

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -21,16 +21,16 @@ class Clean_Tweets:
         drop duplicate rows
         """
         
-        ---
+        duplicated = test[test.astype(str).duplicated()].index
+        df.drop(duplicated, inplace=True)
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        ----
         
-        ----
+        df['created_at'] = pd.to_datetime(df['created_at'], infer_datetime_format=True)
         
         df = df[df['created_at'] >= '2020-12-31' ]
         
@@ -41,10 +41,17 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
+        df['polarity'] = pd.to_numeric(df['polarity'], downcast='float')
         
-        ----
-        ----
+        df['subjectivity'] = pd.to_numeric(df['subjectivity'], downcast='float')
+
+        df['favorite_count'] = pd.to_numeric(df['favorite_count'], downcast='float')
+        
+        df['retweet_count'] = pd.to_numeric(df['retweet_count'], downcast='float')
+        
+        df['followers_count'] = pd.to_numeric(df['followers_count'], downcast='float')
+
+        df['friends_count'] = pd.to_numeric(df['friends_count'], downcast='float')
         
         return df
     
@@ -53,6 +60,21 @@ class Clean_Tweets:
         remove non english tweets from lang
         """
         
-        df = ----
+        df = df[df['lang'] == 'en']
         
         return df
+    
+    def get_cleaned_tweet_data(self)->pd.DataFrame:
+        """
+        uses all functions above and returns cleaned dataframe
+        """
+        
+        first_func = self.drop_unwanted_column(self.df)
+        second_func = self.drop_duplicate(first_func)
+        third_func =self.convert_to_datetime(second_func)
+        fourth_func =self.convert_to_numbers(third_func)
+        fifth_func =self.remove_non_english_tweets(fourth_func)
+        
+        print('\n...Automation complete!')
+        
+        return fifth_func
